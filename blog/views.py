284dda2cbb5157d441b37d79754045861840f57e
Emanuel Angelo <emanuelangelo@outlook.com.br>
2022-09-24 13:31:28 -0300

    usando views baseadas em classe

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # tres postagens em cada pagina
+    paginator = Paginator(object_list, 3) # 3 postagens em cada pagina
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # se a pagina nao for um inteiro do intervalo, exibe a ultima pagina de resultados
+        # se a pagina nao for um inteiro, exibe a primeira pagina
         posts = paginator.page(1)
     except EmptyPage:
-        # se a pgaina estiver fora do intervalo, exibe a ultima pagina de resultados
+        # se a pagina estiver fora do intervalo, exibe a ultima pagina do resultado
         posts = paginator.page(paginator.num_pages)
+    return render(request,
+                 'blog/post/list.html',
+                 {'page': page,
+                  'posts': posts})
 
-    return render(request, 'blog/post/list.html',{'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -25,4 +29,13 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
-    return render(request, 'blog/post/detail.html',{'post': post})
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
