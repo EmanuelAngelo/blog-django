016f3f7b5e572fa7806e0fd8acb2300051585013
Emanuel Angelo <emanuelangelo@outlook.com.br>
2022-09-26 18:37:50 -0300

    criando serviço de envio das postagens por email

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
+from operator import truediv
+from pickle import TRUE
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -39,3 +43,28 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Obtem a postagem com base no id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Formulario foi submetido
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Campos do formulario passaram pela validação
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
